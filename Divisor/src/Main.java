d94761e7b603b522da8228348fce53f6c78920b8
LucyMkr <lucymkrtchyan52@gmail.com>
2023-02-20 20:19:40 +0400

    finding greatest common divisor

---
+++ Divisor/src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+      ForArr myArr = new ForArr(new int[]{ 8, 4, 16, 20, 1 });
+      int smallest = myArr.getSmallest(myArr.arr);
+        System.out.println(myArr.getDivisor(myArr.arr));
+    }
+
+}
+class ForArr{
+    int[] arr;
+    ForArr(int[] a){
+        arr = a;
+    }
+    int getSmallest(int[] theSmallest){
+        int temp;
+        for (int i = 0; i < theSmallest.length; i++)
+        {
+            for (int j = i + 1; j < theSmallest.length; j++)
+            {
+                if (theSmallest[i] > theSmallest[j])
+                {
+                    temp = theSmallest[i];
+                    theSmallest[i] = theSmallest[j];
+                    theSmallest[j] = temp;
+                }
+            }
+        }
+        return theSmallest[0];
+    }
+    int getDivisor(int[] b) {
+        int i;
+        int divisor = 0;
+        for (i = 0; i < getSmallest(b); i++) {
+            if ((double) (b[i] % getSmallest(b)) != 0) {
+                divisor = 1;
+            }else{
+                divisor = getSmallest(b);
+            }
+        }
+        return divisor;
+    }
+}
